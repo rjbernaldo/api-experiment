c8c04b4288b94a0c1a7f858dd81a91f99c658dae
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-04-25 14:33:54 +0800

    Add additional requirements

--- tests/utils/seed.test.js
+++ tests/utils/seed.test.js
@@ -1,8 +1,13 @@
 const test = require('tape');
 const seed = require('../../utils/seed');
+const db = require('../../utils/db');
 
-test('seed data', t => {
-  t.plan(1);
+test('seed', t => {
+  t.plan(2);
   
+  // TODO: should generate data
+  t.ok(false);
+  
+  // TODO: should follow the correct format
   t.ok(false);
 });
