a5689a5eafac650621a0789e5d3afb77212de1e3
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-04-25 16:17:21 +0800

    Make test pass

--- models/Aqi.js
+++ models/Aqi.js
@@ -4,7 +4,10 @@ module.exports = {
   findBetween: (start, end, cb) => {
     _db = db.getDb();
     
-    _db.collection('api-experiment').find({}).toArray((err, docs) => {
+    _db.collection('api-experiment').find({
+      'data.date.time.v': { $gt: start },
+      'data.date.time.v': { $lt: end },
+    }).toArray((err, docs) => {
       cb(docs);
     });
   }
